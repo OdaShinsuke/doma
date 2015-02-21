9287d637b7f52310bc515412caa262d8d1d761a9
shinsuke <stasis@hotmail.com>
2015-02-21 16:49:29 +0900

    MssqlPagingTransformer OFFSET FETCH 対応

--- src/main/java/org/seasar/doma/internal/jdbc/dialect/MssqlPagingTransformer.java
+++ src/main/java/org/seasar/doma/internal/jdbc/dialect/MssqlPagingTransformer.java
@@ -15,15 +15,100 @@
  */
 package org.seasar.doma.internal.jdbc.dialect;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.seasar.doma.internal.jdbc.sql.node.FragmentNode;
+import org.seasar.doma.internal.jdbc.sql.node.OrderByClauseNode;
+import org.seasar.doma.internal.jdbc.sql.node.SelectStatementNode;
+import org.seasar.doma.internal.jdbc.sql.node.WordNode;
+import org.seasar.doma.jdbc.JdbcException;
+import org.seasar.doma.jdbc.SqlNode;
+import org.seasar.doma.message.Message;
+
 
 /**
  * @author taedium
  * 
  */
-public class MssqlPagingTransformer extends Mssql2008PagingTransformer {
+public class MssqlPagingTransformer extends StandardPagingTransformer {
 
     public MssqlPagingTransformer(long offset, long limit) {
         super(offset, limit);
     }
+    
+    @Override
+    public SqlNode visitSelectStatementNode(SelectStatementNode node, Void p) {
+      if (processed) {
+          return node;
+      }
+      processed = true;
 
+      OrderByClauseNode originalOrderBy = node.getOrderByClauseNode();
+      if (originalOrderBy == null) {
+          throw new JdbcException(Message.DOMA2201);
+      }
+      
+      OrderByClauseNode orderBy = new OrderByClauseNode(originalOrderBy.getWordNode());
+      List<SqlNode> preNodes = new ArrayList<>();
+      List<SqlNode> postNodes = new ArrayList<>();
+      for (SqlNode child : originalOrderBy.getChildren()) {
+          if (!postNodes.isEmpty()) {
+              postNodes.add(child);
+          } else if (startsPostNode(child)) {
+              // pre の末尾の WhitespaceNode を post に移す
+              SqlNode last = preNodes.remove(preNodes.size() - 1);
+              postNodes.add(last);
+              postNodes.add(child);
+          } else {
+              preNodes.add(child);
+          }
+      }
+      for (SqlNode preNode : preNodes) {
+          orderBy.appendNode(preNode);
+      }
+      
+      String offset = this.offset <= 0 ? "0" : String.valueOf(this.offset);
+      
+      orderBy.appendNode(new FragmentNode(" offset "));
+      orderBy.appendNode(new FragmentNode(offset));
+      orderBy.appendNode(new FragmentNode(" rows"));
+      if (this.limit > 0) {
+          orderBy.appendNode(new FragmentNode(" fetch next "));
+          orderBy.appendNode(new FragmentNode(String.valueOf(this.limit)));
+          orderBy.appendNode(new FragmentNode(" rows only"));
+      }
+      
+      for (SqlNode postNode : postNodes) {
+          orderBy.appendNode(postNode);
+      }
+      
+      if (node.getForUpdateClauseNode() != null) {
+          orderBy.appendNode(new FragmentNode(" "));
+      }
+      
+      SelectStatementNode result = new SelectStatementNode();
+      result.setSelectClauseNode(node.getSelectClauseNode());
+      result.setFromClauseNode(node.getFromClauseNode());
+      result.setWhereClauseNode(node.getWhereClauseNode());
+      result.setGroupByClauseNode(node.getGroupByClauseNode());
+      result.setHavingClauseNode(node.getHavingClauseNode());
+      result.setOrderByClauseNode(orderBy);
+      result.setForUpdateClauseNode(node.getForUpdateClauseNode());
+      return result;
+    }
+    
+    /**
+     * OrderByClauseNode に含まれる ソート順指定以外のノードを検出する。(FOR 句/OPTION 句)
+     * @param node
+     * @return ソート順以外のノードを検出したら true.それ以外は false.
+     */
+    protected boolean startsPostNode(SqlNode node) {
+      if (!(node instanceof WordNode)) {
+         return false;
+      }
+      String word = ((WordNode)node).getWord();
+      return "for".equalsIgnoreCase(word) || 
+          "option".equalsIgnoreCase(word);
+    }
 }
